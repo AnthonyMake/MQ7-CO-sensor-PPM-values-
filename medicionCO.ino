8b8f51febc5d00544cfdcf37a6ee9f39cdcc26ff
Antonio Vásquez Briones <niox33@gmail.com>
2017-11-17 13:44:15 -0300

    Programa de Prueba
    Permite modificar los tiempos con mayor facilidad y mostrar en el monitor serial lo que esta sucediendo.

---
+++ medicionCO.ino
@@ -0,0 +1,28 @@
+#define sensorSupply 9
+#define sensorMeas   0
+#define t5v     10 //en segundos
+#define t14v    10
+int vSense;
+
+void setup() {
+  pinMode(sensorSupply,OUTPUT);
+
+  Serial.begin(9600);
+}
+
+
+void loop() {
+  for(int i=0;i<=t5v;i++){
+     delay(999);
+     Serial.print(".");
+  }
+  Serial.println();
+
+  for(int i=0;i<=t14v;i++){
+     delay(999);
+     vSense=analogRead(sensorMeas);
+     Serial.print("medicion: ");
+     Serial.println(vSense);
+  }
+  
+}
